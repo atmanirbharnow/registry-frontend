4ac00d6f727ddc5d4fd74a6ede7bdeeea7d0a564
Amitsinh Vaghela <info@earthcarbonfoundation.org>
2025-12-28 23:08:19 +0530

    Fix next.config.js to CommonJS for Firebase build

--- next.config.js
+++ next.config.js
@@ -1,8 +1,6 @@
-// next.config.mjs
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  // No experimental block needed for basic app
-  // Only add serverExternalPackages if using Firebase Admin in Server Actions
-}
+  reactStrictMode: true,
+};
 
-export default nextConfig;
+module.exports = nextConfig;
